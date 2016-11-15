78b7ceffb91a2255d33db382a8c25981032516d2
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2016-11-15 20:29:37 +0100

    Finish add/edit/delete/list Category Event
    Partie Catégorie fini

--- src/AppBundle/Controller/backOffice/TypeEventController.php
+++ src/AppBundle/Controller/backOffice/TypeEventController.php
@@ -20,7 +20,9 @@ class TypeEventController extends Controller
     {
         $typeEvents = $this->getDoctrine()
             ->getRepository(Typeevent::class)
-            ->findAll();
+//            ->findAll();
+            ->findBy(array(), array('libelleType' => 'asc')); // Order by ASC
+
         return $this->render('@App/backOffice/listTypeEvent.html.twig', ['typeEvents' => $typeEvents]);
     }
 
@@ -62,5 +64,62 @@ class TypeEventController extends Controller
         // On rend la vue
         return $this->render('@App/backOffice/createTypeEvent.html.twig', ['form' => $formview]);
 
+    }
+
+    /**
+     * @Route("/admin/category/edit/{id}", name="admin_category_edit", requirements={"id" = "\d+"})
+     * @param $typeEvent
+     * @return Response
+     */
+    public function editAction(Request $request, Typeevent $typeEvent)
+    {
+
+        // On récupère le formulaire
+        $form = $this->createForm(TypeeventType::class, $typeEvent);
+
+        // Requête envoyée par le formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire a été soumi et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On enregistre la catégorie en BDD
+            $em = $this->getDoctrine()->getManager();
+
+//          $em->persist($typeEvent); // inutile car l'objet provient déjà de la bdd
+            $em->flush();
+
+            // Flash message
+            $this->addFlash('success', 'Catégorie modifiée avec succès.');
+
+            return $this->redirectToRoute('admin_category_list');
+        }
+
+        // On génère le html du formulaire crée
+        $formview = $form->createView();
+
+        // On rend la vue
+        return $this->render('@App/backOffice/editTypeEvent.html.twig', ['form' => $formview]);
+    }
+
+    /**
+     * @Route("/admin/category/delete/{id}", name="admin_category_delete", requirements={"id" = "\d+"})
+     * @param $typeEvent
+     * @return Response
+     */
+    public function deleteAction(Request $request, Typeevent $typeEvent)
+    {
+
+        // On enregistre la catégorie en BDD
+        $em = $this->getDoctrine()->getManager();
+
+        // On supprime la catégorie dans la bdd
+        $em->remove($typeEvent);
+        $em->flush();
+
+        // Flash message
+        $this->addFlash('success', 'Catégorie supprimée avec succès.');
+
+        return $this->redirectToRoute('admin_category_list');
     }
 }
