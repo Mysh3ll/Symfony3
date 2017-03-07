5ee1a51981da1b98b38b9187be1fda6a269a99a3
Mysh3ll <michel.pompas@greta-sud-aquitaine.fr>
2017-03-07 21:45:18 +0100

    push vers origin master

--- src/RestApiBundle/Controller/UserController.php
+++ src/RestApiBundle/Controller/UserController.php
@@ -9,23 +9,45 @@ use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use AppBundle\Entity\User;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
 
 class UserController extends Controller
 {
     /**
      * @Rest\View()
-     * @Rest\Get("/users")
+     * @Rest\Post("/login")
      * @param Request $request
-     * @return User[]|array
+     * @return object
      */
-    public function getUsersAction(Request $request)
+    public function postLoginAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $users = $em->getRepository('AppBundle:User')->findAll();
+        $user = $this->getDoctrine()
+            ->getRepository('AppBundle:User')
+            ->findOneBy(['username' => $request->get('username')]);
+        if (!$user) {
+//            throw $this->createNotFoundException();
+            return ['message' => 'Utilisateur non trouvé.',
+                    'success' => 0];
+        }
+        $isValid = $this->get('security.password_encoder')
+            ->isPasswordValid($user, $request->get('password'));
+        if (!$isValid) {
+//            throw new BadCredentialsException();
+            return ['message' => 'Mauvais mot de passe !',
+                    'success' => 0];
+        }
+        $token = $this->get('lexik_jwt_authentication.encoder')
+            ->encode([
+                'username' => $user->getUsername(),
+                'exp'      => time() + 3600 // 1 hour expiration
+            ]);
 
-        /* @var $users User[] */
+        $role = $user->getRoles();
 
-        return $users;
+        return ['token'   => $token,
+                'success' => 1,
+                'message' => 'Connecté.',
+                'role'    => $role];
     }
 
     /**
@@ -47,64 +69,40 @@ class UserController extends Controller
         return $user;
     }
 
-    /**
-     * @Rest\View()
-     * @Rest\Post("/users/login")
-     * @param Request $request
-     * @return object
-     */
-    public function postUserAction(Request $request)
+    protected function getCredentials(Request $request)
     {
-        $userName = $request->get('username');
-        $password = $request->get('password');
 
-        $user = $this->getDoctrine()
-            ->getRepository('AppBundle:User')
-            ->findOneBy(['username' => $userName]);
+        if (!$request->headers->has('Authorization')) {
+            return;
+        }
+
+        $extractor = new AuthorizationHeaderTokenExtractor(
+            'Bearer',
+            'Authorization'
+        );
+
+        $token = $extractor->extract($request);
+
+        if (!$token) {
+            return;
+        }
+
+        return $token;
+    }
+
+    protected function checkUser($credentials)
+    {
+        $data = $this->get('lexik_jwt_authentication.encoder')
+            ->decode($credentials);
+        $username = $data['username'];
+
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')
+            ->findOneBy(['username' => $username]);
 
         if (!$user) {
-            return new JsonResponse(['message' => 'User not found.'], Response::HTTP_NOT_FOUND);
+            return;
         }
 
-        $isValid = $this->get('security.password_encoder')
-            ->isPasswordValid($user, $password);
-
-        if (!$isValid) {
-            return new JsonResponse(['message' => 'Wrong password !'], Response::HTTP_NOT_FOUND);
-        }
-
-        $token = $this->getToken($user);
-
-        return new JsonResponse(['token' => $token], Response::HTTP_OK);
-    }
-
-    /**
-     * Returns token for user.
-     *
-     * @param User $user
-     *
-     * @return array
-     */
-    public function getToken(User $user)
-    {
-        return $this->container->get('lexik_jwt_authentication.encoder')
-            ->encode([
-                'username' => $user->getUsername(),
-                'exp'      => $this->getTokenExpiryDateTime(),
-            ]);
-    }
-
-    /**
-     * Returns token expiration datetime.
-     *
-     * @return string Unixtmestamp
-     */
-    private function getTokenExpiryDateTime()
-    {
-        $tokenTtl = $this->container->getParameter('lexik_jwt_authentication.token_ttl');
-        $now = new \DateTime();
-        $now->add(new \DateInterval('PT' . $tokenTtl . 'S'));
-
-        return $now->format('U');
+        return $user;
     }
 }
